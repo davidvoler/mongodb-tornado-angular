f66dc5192497024b8172002cbd046ce9b2ed0836
Avraham Seror <avrahams@crow.co.il>
2014-09-21 16:06:35 +0300

    PEP8

--- src/server/app.py
+++ src/server/app.py
@@ -11,21 +11,21 @@ import pymongo
 from tornado.options import define, options
 from tornado import ioloop, web
 
-#adding local directory to path
+# adding local directory to path
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 define("port", default=9915, help="port", type=int)
-define("mongodb_host",default='localhost:27017', help='Monogo Database Host', type=str)
-define("mongodb_name",default='mat', help='Database Name', type=str)
+define("mongodb_host", default='localhost:27017', help='Monogo Database Host', type=str)
+define("mongodb_name", default='mat', help='Database Name', type=str)
 
 #read configuration from default configuration file
 options.parse_config_file(
-    os.path.join(os.path.dirname(os.path.realpath(__file__)),'settings.py'),
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.py'),
     False)
 #if a local settings exists get information from settings
 try:
     tornado.options.parse_config_file(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)),'local_settings.py'),
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), 'local_settings.py'),
         False)
 except Exception as e:
     #we just use the default settings
@@ -39,24 +39,26 @@ class IndexHandler(web.RequestHandler):
     def get(self):
         self.render("../templates/index.html")
 
+
 class ApiHandler(tornado.web.RequestHandler):
     def initialize(self, db):
         self._db = db
 
     def get(self):
         self.render("../templates/index.html")
-app = tornado.web.Application([
-                          (r'/', IndexHandler),
-                          (r'/tf', ApiHandler, dict(db=db))
-                      ],
-                      static_path=os.path.join(os.path.dirname(__file__), '../client/'),
-                      autoreload=True
+
+
+app = tornado.web.Application([(r'/', IndexHandler),
+                               (r'/tf', ApiHandler, dict(db=db))
+                              ],
+                              static_path=os.path.join(os.path.dirname(__file__), '../client/'),
+                              autoreload=True
 )
 
 if __name__ == '__main__':
     #read settings from commandline
     options.parse_command_line()
     print ('server running on http://localhost:{}'.format(options.port))
-    app.listen(options.port,xheaders=True)
+    app.listen(options.port, xheaders=True)
     ioloop = tornado.ioloop.IOLoop.instance()
     ioloop.start()
